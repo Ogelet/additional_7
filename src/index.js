44191ef667e95e6c6a1b75e73b3cd6f9a792ed14
Svetlana Telego <camarezi@gmail.com>
2018-03-14 01:39:59 +0300

    some solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
-module.exports = function solveSudoku(matrix) {
+module.exports = function solveSudoku(matrix){
   let result = false;
   let newMatrix = matrix;
   stagesArray = [];
   function check (number, row, column) {
     if(!result){
-    console.log('RESSUULLTTTTTTTTTTTTTTTTTT!!!!!!!!!!!!! ', result)
-    function checkRow (){
+    function checkRowColumn (){
       for( i=0; i<=8; i++){
-        console.log(newMatrix,newMatrix[row],'i',i,'newMatrix[row][i]', newMatrix[row][i],'number',number)
-        if(newMatrix[row][i] === number){
-          console.log('check row NOooo', 'newMatrix[row][i]', newMatrix[row][i], 'number', number );
+        if(newMatrix[row][i] === number || newMatrix[i][column] === number){
           getNumber(row, column, number+1);
           return false;
         } else if(i === 8){
-          console.log('checked row - TRUE');
-          return true};
-      }
-    };
-    function checkColumn (){
-      for( i=0; i<=8; i++){
-        if(newMatrix[i][column] === number){
-          console.log('check column NOooo', 'newMatrix[row][i]', newMatrix[i][column], 'number', number );
-          getNumber(row, column, number+1);
-          return false;
-        } else if(i === 8){
-          console.log('checked COLUMN - TRUE');
           return true};
       }
     };
@@ -33,74 +18,105 @@ module.exports = function solveSudoku(matrix) {
       const segment_row2 = segment_row1 + 2;
       const segment_column1 = Math.floor(column/3) * 3;
       const segment_column2 = segment_column1 + 2;
-      /* console.log('segment', 'segment_row1 --- ', segment_row1, 'segment_row2 ---', segment_row2, 'segment_column1', segment_column1 ,'segment_column2', segment_column2); */
       for( i=segment_row1; i<=segment_row2; i++){
         for( j=segment_column1; j<=segment_column2; j++){
           if(newMatrix[i][j] === number){
-          console.log('----Segment NOooo', 'check segment - i,j    ', i  ,   j , 'number', number );
           getNumber(row, column, number+1);
           return false;
       } else if(i === segment_row2 && j === segment_column2){    
-        console.log('checked SEGMENT - TRUE');
         return true};
     } 
     };
     };
-    console.log('!!!!!!!!!!!!!!!!!!!!!!checkRow() && checkColumn() && checkSegment()', checkRow() && checkColumn() && checkSegment(),'number', number);
-    if(checkRow() && checkColumn() && checkSegment()){
-      console.log(' --------------------------------true---------------------------------------');
+    if(checkRowColumn() && checkSegment()){
+      // console.log('-checked all - it is true-');
       return true
-    } else {console.log(' --------------------------------true---------------------------------------');
+    } else {//console.log(' --- checked all - it is false');
     return false}
   }};
   function backstage(){
-    console.log('',' NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN', '!!!!!!!!!!!!!!!!!!!!!!!!','stagesArray', stagesArray);
+    if(stagesArray.length > 0 ){
+    // console.log('',' NNNNNNNNNNNNNN', '!!!!!!!!!!!!!!!!!!!!!!!!','stagesArray', stagesArray, stagesArray.length);
     let row = stagesArray[stagesArray.length - 2][0];
     let column = stagesArray[stagesArray.length - 2][1];
     let number = stagesArray[stagesArray.length - 2][2] + 1;
     stagesArray.pop();
-    console.log('row', row,'column', column,'number', number,'stagesArray', stagesArray);
-    getNumber(row, column, number);
+    if(number === 10){
+      // console.log('row', row,'column', column,'number', number,'stagesArray', stagesArray);
+      newMatrix[row][column] = 0;
+      backstage();
+    } else {
+      // console.log('row', row,'column', column,'number', number,'stagesArray', stagesArray);
+      getNumber(row, column, number);
+    }
+    }
   }
   function getNumber(i, j, number = 1){
+    if(checkZero()){
       for(let n = number; n < 10; n++){
-        if(!result){
-        console.log('NUMBER', n, 'getNumber');
+        if(checkZero()){
         if(check(n,i,j)){
           newMatrix[i][j] = n;
-          console.log("AAAAAAAAAAAAAAAAAAAAA", newMatrix);
           stagesArray[stagesArray.length - 1][2] = n;
-          findZero();
+          if(!checkZero()){
+            return
+          } else {
+            findZero();
+          }
+         
         } else {
-          console.log('=============================here=========================');
-          backstage();
+          if(!checkZero()){
+            // console.log('trueeeee', result, newMatrix);
+            return
+          } else {
+            // console.log('=======   here we go on backstage() ============','i - ', i, 'j', j, stagesArray, '!!!!!!!!!!!!', 'result', result );
+            newMatrix[i][j]=0;
+            backstage();
+          }
         }
-      }    
+      }
     }
+   } else
+   {
+    // console.log('new matrixxxxxx', newMatrix)
+   }
+
   };
-  findZero();
+    
+  function checkZero(){
+    let zerosMatrix = false;
+    for( let i=0; i<=8; i++){
+    for( let j=0; j<=8; j++){
+      if (newMatrix[i][j] === 0){
+        zerosMatrix = true;
+      }
+    }
+    }
+    return zerosMatrix;
+  }
   function findZero(){
     for( i=0; i<=8; i++){ if(result === false){
       for( j=0; j<=8; j++) { if (result === false){
        // console.log(newMatrix, i, j, newMatrix[i][j]);
         if (newMatrix[i][j] === 0){
           stagesArray.push([i,j,0]);
-          console.log('findZero', 'newMatrix.indexOf(0)>-1', 'i=', i, 'j=', j, 'stagesArray', stagesArray);
+          // console.log('findZero','stagesArray', stagesArray);
           getNumber(i, j);
         } else if(i === 8 && j === 8){
-          console.log('NOTTTT findZero', 'newMatrix.indexOf(0)>-1', 'i=', i, 'j=', j);
+         // console.log('NOTTTT findZero');
           result = true;
-          return result;
+          break;
         }
       } else {return};
     } 
     } else {return};
     };
   };
-  console.log(newMatrix, stagesArray);
-  if (result === true){
-    return newMatrix;
+
+  if(checkZero()){
+    findZero();
   };
+  return newMatrix;
 };
 
 
