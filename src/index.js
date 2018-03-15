4e6af552b9f1ce5487bd70ec5b9563c5cba3e4e5
Svetlana Telego <camarezi@gmail.com>
2018-03-15 10:46:46 +0300

    solution 30%

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 module.exports = function solveSudoku(matrix){
-  let result = false;
-  let newMatrix = matrix;
   stagesArray = [];
+
   function check (number, row, column) {
-    if(!result){
+    // console.log('function check');
     function checkRowColumn (){
+      // console.log('function checkRowColumn');
       for( i=0; i<=8; i++){
-        if(newMatrix[row][i] === number || newMatrix[i][column] === number){
+        if(matrix[row][i] === number || matrix[i][column] === number){
           getNumber(row, column, number+1);
           return false;
         } else if(i === 8){
@@ -14,111 +14,94 @@ module.exports = function solveSudoku(matrix){
       }
     };
     function checkSegment (){
+      // console.log('function checkSegment');
       const segment_row1 = Math.floor(row/3) * 3;
       const segment_row2 = segment_row1 + 2;
       const segment_column1 = Math.floor(column/3) * 3;
       const segment_column2 = segment_column1 + 2;
       for( i=segment_row1; i<=segment_row2; i++){
         for( j=segment_column1; j<=segment_column2; j++){
-          if(newMatrix[i][j] === number){
+          if(matrix[i][j] === number){
           getNumber(row, column, number+1);
           return false;
       } else if(i === segment_row2 && j === segment_column2){    
         return true};
-    } 
+    }};
     };
-    };
-    if(checkRowColumn() && checkSegment()){
-      // console.log('-checked all - it is true-');
-      return true
-    } else {//console.log(' --- checked all - it is false');
-    return false}
-  }};
+    if(checkRowColumn() && checkSegment()){ return true
+    } else { return false }
+  };
+
   function backstage(){
-    if(stagesArray.length > 0 ){
-    // console.log('',' NNNNNNNNNNNNNN', '!!!!!!!!!!!!!!!!!!!!!!!!','stagesArray', stagesArray, stagesArray.length);
-    let row = stagesArray[stagesArray.length - 2][0];
-    let column = stagesArray[stagesArray.length - 2][1];
-    let number = stagesArray[stagesArray.length - 2][2] + 1;
-    stagesArray.pop();
+    // console.log('function backstage');
+    const aLenght = stagesArray.length - 1;
+    if(aLenght >= 0 ){
+    let row = stagesArray[aLenght][0];
+    let column = stagesArray[aLenght][1];
+    let number = stagesArray[aLenght][2] + 1;
+    // console.log(row, 'row', column, 'column', number, 'number');
     if(number === 10){
-      // console.log('row', row,'column', column,'number', number,'stagesArray', stagesArray);
-      newMatrix[row][column] = 0;
+      matrix[row][column] = 0;
+      stagesArray.pop();
       backstage();
     } else {
-      // console.log('row', row,'column', column,'number', number,'stagesArray', stagesArray);
       getNumber(row, column, number);
     }
-    }
-  }
+  }};
+
   function getNumber(i, j, number = 1){
-    if(checkZero()){
-      for(let n = number; n < 10; n++){
-        if(checkZero()){
-        if(check(n,i,j)){
-          newMatrix[i][j] = n;
-          console.log(n);
+    // console.log('function getNumber');
+    for(let n = number; n < 10; n++){
+      
+      if(check(n,i,j)){
+          matrix[i][j] = n;
           stagesArray[stagesArray.length - 1][2] = n;
           if(!checkZero()){
             return
           } else {
             findZero();
-          }
-         
-        } else {
-          if(!checkZero()){
-            // console.log('trueeeee', result, newMatrix);
-            return
-          } else {
-            // console.log('=======   here we go on backstage() ============','i - ', i, 'j', j, stagesArray, '!!!!!!!!!!!!', 'result', result );
-            newMatrix[i][j]=0;
+          }       
+      } else {
+          if(checkZero()){
+            matrix[i][j]=0;
+            stagesArray.pop();
             backstage();
+          } else {
+            return
           }
-        }
       }
     }
-   } else
-   {
-    // console.log('new matrixxxxxx', newMatrix)
-   }
-
   };
-    
+
   function checkZero(){
-    let zerosMatrix = false;
+    // console.log('function checkZero');
     for( let i=0; i<=8; i++){
-    for( let j=0; j<=8; j++){
-      if (newMatrix[i][j] === 0){
-        zerosMatrix = true;
+      if(matrix[i].includes(0)){
+        return  true;
       }
     }
-    }
-    return zerosMatrix;
-  }
+  };
+
   function findZero(){
-    for( i=0; i<=8; i++){ if(result === false){
-      for( j=0; j<=8; j++) { if (result === false){
-       // console.log(newMatrix, i, j, newMatrix[i][j]);
-        if (newMatrix[i][j] === 0){
+    // console.log('function findZero');
+    for( i=0; i<=8; i++){
+      for( j=0; j<=8; j++){
+        if (matrix[i][j] === 0){
           stagesArray.push([i,j,0]);
-          // console.log('findZero','stagesArray', stagesArray);
           getNumber(i, j);
-        } else if(i === 8 && j === 8){
-         // console.log('NOTTTT findZero');
-          result = true;
+        } else if (i === 8 && j === 8){
           break;
         }
-      } else {return};
-    } 
-    } else {return};
-    };
+      }
+    }
   };
 
   if(checkZero()){
     findZero();
   };
-  console.log(newMatrix);
-  return newMatrix;
+
+  console.log(matrix);
+  return matrix;
 };
 
 
